30eb1ed1f7c40882f58de682e40db5e40a59c140
b21isabe <129263151+b21isabe@users.noreply.github.com>
2025-03-11 11:19:29 +0100

    created database
    created tables and inserted some test data

---
+++ weatherDB.sql
@@ -0,0 +1,54 @@
+DROP DATABASE weatherDB;
+CREATE DATABASE weatherDB;
+USE weatherDB;
+
+CREATE TABLE cities (
+    city_name VARCHAR(30),
+	latitude DECIMAL(9,6),
+    longitude DECIMAL(9,6),
+    station_name VARCHAR (100),
+    station_id BIGINT,
+    elevation DECIMAL(6,2),
+    PRIMARY KEY (station_id)
+)engine=innodb;
+
+CREATE TABLE weather_data (
+    station_id BIGINT,
+    observation_time DATETIME,
+    wind_direction INT,
+    wind_speed DECIMAL(5,2),
+    visibility INT,
+    temperature DECIMAL(5,2),
+    dew_point DECIMAL(5,2),
+    atmospheric_pressure DECIMAL(6,2),
+    ceiling INT,
+    PRIMARY KEY (station_id, observation_time),
+    FOREIGN KEY (station_id) REFERENCES cities(station_id)
+)engine=innodb;
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/city_info.csv'
+INTO TABLE cities
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(city_name,	latitude, longitude, station_name, station_id, elevation);
+    
+
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/beijingtest.csv'
+INTO TABLE weather_data
+FIELDS TERMINATED BY ','  
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(observation_time, @wind_direction, @wind_speed, @visibility, @temperature, @dew_point, @atmospheric_pressure, @ceiling)
+SET 
+    wind_direction = NULLIF(@wind_direction, ''),
+    wind_speed = NULLIF(@wind_speed, ''),
+    visibility = NULLIF(@visibility, ''),
+    temperature = NULLIF(@temperature, ''),
+    dew_point = NULLIF(@dew_point, ''),
+    atmospheric_pressure = NULLIF(@atmospheric_pressure, ''),
+    ceiling = NULLIF(@ceiling, ''),
+    station_id = 54511099999;
+    
