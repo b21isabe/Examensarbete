453ff9fdab1810aacf746489f15d41714ec17847
b21isabe <129263151+b21isabe@users.noreply.github.com>
2025-03-18 17:00:28 +0100

    created index.js for graphql

---
+++ graphql api/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+
+const app = express();
+const PORT = 3000;
+
+app.use("/graphql", graphqlHTTP({
+  graphiql: true
+}));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
