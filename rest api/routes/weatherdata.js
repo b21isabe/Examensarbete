586d33da7f1de6b6f69de280b2d41f2ad4628479
b21isabe <129263151+b21isabe@users.noreply.github.com>
2025-03-29 17:24:26 +0100

    made rest api able to fetch specific data
    It now fetches data from a station_id and a start_date and the chosen number of days after that

--- rest api/routes/weatherdata.js
+++ rest api/routes/weatherdata.js
@@ -3,8 +3,18 @@ const db = require("../db");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    //temporary select query, so we dont load in 1 million rows of data
-    db.query("SELECT * FROM weather_data WHERE observation_time = '2020/12/12 00:00:00'", (err, results) => {
+
+    const { station_id, start_date, days } = req.query;
+    const parsedStationId = parseInt(station_id, 10);
+    const parsedDays = parseInt(days, 10);
+
+    const query = `
+        SELECT * FROM weather_data
+        WHERE station_id = ? AND observation_time >= ? AND observation_time < DATE_ADD(?, INTERVAL ? DAY);
+        `;
+    
+
+    db.query(query,[parsedStationId, start_date, start_date, parsedDays], (err, results) => {
         if (err) return res.status(500).json({ error: err.message });
         res.json(results);
     });
