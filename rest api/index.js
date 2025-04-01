bd7c9be26297a436f2f3579abf3be3ea17a1758a
b21isabe <129263151+b21isabe@users.noreply.github.com>
2025-04-01 20:18:32 +0200

    added cors to rest api

--- rest api/index.js
+++ rest api/index.js
@@ -3,9 +3,12 @@ const express = require("express");
 const db = require("./db");
 const citiesRoutes = require("./routes/cities");
 const weatherRoutes = require("./routes/weatherdata");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.use(express.json()); 
 app.use("/cities", citiesRoutes);
