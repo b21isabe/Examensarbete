61a36f88bd040e2a250e76f38890bd842e35e20e
b21isabe <129263151+b21isabe@users.noreply.github.com>
2025-04-21 20:41:58 +0200

    fixed rest api cities route
    now allows seaches with station id

--- rest api/routes/cities.js
+++ rest api/routes/cities.js
@@ -3,10 +3,19 @@ const db = require("../db");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    db.query("SELECT * FROM cities", (err, results) => {
-        if (err) return res.status(500).json({ error: err.message });
-        res.json(results);
-    });
+    const station_id = req.query.station_id;
+
+    if (station_id) {
+        db.query("SELECT * FROM cities WHERE station_id = ?", [station_id], (err, results) => {
+            if (err) return res.status(500).json({ error: err.message });
+            res.json(results[0]);
+        });
+    } else {
+        db.query("SELECT * FROM cities", (err, results) => {
+            if (err) return res.status(500).json({ error: err.message });
+            res.json(results);
+        });
+    }
 });
 
 module.exports = router;
