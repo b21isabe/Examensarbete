c91b1e69435463937551487e3981ff2c0dfb256e
b21isabe <129263151+b21isabe@users.noreply.github.com>
2025-05-15 16:10:19 +0200

    updated cities route to handle city names

--- rest api/routes/cities.js
+++ rest api/routes/cities.js
@@ -3,10 +3,15 @@ const db = require("../db");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    const station_id = req.query.station_id;
+    const { station_id, city_name } = req.query;
 
     if (station_id) {
         db.query("SELECT * FROM cities WHERE station_id = ?", [station_id], (err, results) => {
+            if (err) return res.status(500).json({ error: err.message });
+            res.json(results[0]);
+        });
+    } else if (city_name) {
+        db.query("SELECT * FROM cities WHERE city_name = ?", [city_name], (err, results) => {
             if (err) return res.status(500).json({ error: err.message });
             res.json(results[0]);
         });
